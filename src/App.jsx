373bfad65ee86893b778899b18aa55cd452cc3d8
lstew111 <loren.stewart@engelvoelkers.com>
2024-07-15 12:36:44 -0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+const imageObjects = [
+  {
+    url: 'https://images.unsplash.com/photo-1600804340584-c7db2eacf0bf?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'an image of a puppy',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1583511655826-05700d52f4d9?q=80&w=2488&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'an image of a puppy',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1600804340584-c7db2eacf0bf?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'an image of a puppy',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1601979031925-424e53b6caaa?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'an image of a puppy',
+  },
+  {
+    url: 'https://plus.unsplash.com/premium_photo-1669040674572-836c64a3ac70?q=80&w=2265&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'an image of a puppy',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1593134257782-e89567b7718a?q=80&w=2448&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'an image of a puppy',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1510337550647-e84f83e341ca?q=80&w=2231&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'an image of a puppy',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1591160690555-5debfba289f0?q=80&w=2264&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'an image of a puppy',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1618359057154-e21ae64350b6?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'an image of a puppy',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1560132248-d946352460e3?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'an image of a puppy',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1535914191444-2a3b177956c7?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'an image of a puppy',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1546527868-ccb7ee7dfa6a?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'an image of a puppy',
+  },
+];
+
+/*
+
+In the return, inside the <main> tag, map over the imageObjects array. For each image,
+create the following html.
+
+<div>
+  <img />
+</div>
+
+On the image, set `src` to the image url, and set the
+img's alt to the `alt` to the alt property in the imageObject
+
+*/
+
+function App() {
+  return <main></main>;
+}
+
+export default App;
